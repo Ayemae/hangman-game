2489f2ea25f3dafdbc8ca3685342908720da48e4
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-06-15 21:54:05 -0400

    Ooooooo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 var Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var placesToHang = [
-    "amusement park", "aquarium", "barbeque", "bird watching", "bookstore", "bon fire", "brewery", "camping",
+    "a back alley", "amusement park", "aquarium", "barbeque", "bird watching", "bookstore", "bon fire", "brewery", "camping",
     "coffee shop", "convention", "cow tipping", "fishing", "ice cream parlor", "hibachi", "hiking", "ikea",
     "lan party", "museum", "music festival", "my place", "petting zoo", "pinball arcade", "pizza parlor",
     "ren fair", "restaurant", "tea room", "the beach", "the city", "the local bar", "the mall",
-    "the movies", "the park", "the zoo", "shopping", "staples", "sushi bar", "whale watching", "your place"
+    "the movies", "the park", "the zoo", "scavenger hunt", "shopping", "staples", "sushi bar", "water park",
+    "whale watching", "your place"
 ];
 var mysteryWord = undefined;
 var userGuess = undefined;
@@ -16,9 +17,18 @@ var remainingGuesses = 7;
 var userGuessArray = [];
 
 var errorMessage = " ";
+var outcomeMessage = " ";
+
+function gameClear() {
+    remainingGuesses = 7;
+    userGuessArray = [];
+    letterSlots = [];
+    startNewGame();
+};
 
 
 function startNewGame() {
+
     var mysteryWord = placesToHang[Math.floor(Math.random() * placesToHang.length)]
 
     for (var i = 0; i < mysteryWord.length; i++) {
@@ -28,7 +38,8 @@ function startNewGame() {
         else {
             letterSlots[i] = "&nbsp;";
         }
-    } 
+    }
+
 
     console.log("Computer's word: " + mysteryWord);
     console.log(letterSlots);
@@ -50,7 +61,6 @@ function startNewGame() {
                 else {
                     for (var j = 0; j < mysteryWord.length; j++) {
                         if (mysteryWord[j] === userGuess) {
-                            console.log("I see you!");
                             letterSlots[j] = userGuess;
                         }
                     }
@@ -58,18 +68,17 @@ function startNewGame() {
 
                 if (letterSlots.indexOf("_") === -1) {
                     wins++;
-                    remainingGuesses = 7;
-                    userGuessArray = [];
-                    letterSlots = [];
-                    startNewGame();
+                    outcomeMessage = "Success! <br/> Press ENTER to play again."
                 }
 
                 if (remainingGuesses === 0) {
                     losses++;
-                    remainingGuesses = 7;
-                    userGuessArray = [];
-                    letterSlots = [];
-                    startNewGame();
+                    for (var k = 0; k < letterSlots.length; k++) {
+                        if (letterSlots[k] == "_") {
+                            letterSlots[k] = mysteryWord[k].fontcolor("#aaaaaa");
+                        }
+                        outcomeMessage = "Your social life is dead. :( <br/> Press ENTER to play again."
+                    }
                 }
             } else {
                 errorMessage = "<p> ! You already guessed that letter. </p>";
@@ -91,7 +100,10 @@ addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
         addEventListener("keyup", function (event) {
             if (event.keyCode === 13) {
-                console.log("lol")
+                remainingGuesses = 7;
+                userGuessArray = [];
+                letterSlots = [];
+                outcomeMessage = " ";
                 startNewGame();
             }
 
@@ -102,11 +114,15 @@ addEventListener("keydown", function (event) {
                 "<p>Losses: " + losses + "</p>" +
                 "<p>Remaining Guesses: " + remainingGuesses + "</p>";
 
+
+            var outcomehtml = outcomeMessage;
+
             var errorhtml = errorMessage;
 
             var letterSlotshtml = letterSlots.join(" ");
             document.querySelector("#game-space").innerHTML = letterSlotshtml;
 
+            document.querySelector("#outcome").innerHTML = outcomehtml;
             document.querySelector("#guesses").innerHTML = guesseshtml;
             document.querySelector("#game").innerHTML = statshtml;
             document.querySelector("#error").innerHTML = errorhtml;
